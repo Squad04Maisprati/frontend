8414c60d0c28d8396c9c7017cf4d4274104240f0
Gustavo Cigaran <gustavocigaran@gmail.com>
2025-02-07 21:30:57 -0300

    Adicionando rota para CadastroBanco

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ContasPagar from './components/ContasPagar/ContasPagar';
 import ContasReceber from './components/ContasReceber/ContasReceber';
+import CadastroBanco from './components/CadastroBanco/CadastroBanco';
 import EmergencySavings from './components/EmergencySavings/EmergencySavings';
 import Header from './components/Header'
 // import RegistrationPage from './components/Registration/RegistrationPage'
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/contas-pagar" element={<ContasPagar />} />
         <Route path="/contas-receber" element={<ContasReceber />} />
+        <Route path="/cadastro-banco" element={<CadastroBanco />} />
         <Route path="/emergency-savings" element={<EmergencySavings />} />
          {/* <div className="App">
         <RegistrationPage />
