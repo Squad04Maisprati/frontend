d5be107d0295d2fe17522b1e89871b7b2333af2c
MatheusKassaoka <mkassaoka@gmail.com>
2025-03-11 17:05:21 -0300

    Atualizando a tela de login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,36 +1,71 @@
-import React from 'react'
-import "./Login.css" 
+import React from 'react';
+import { useState } from 'react';
+import './Login.css';
 
-const Login = () => {
+function Login() {
+  const [firstName, setFirstname] = useState('');
+  const [password, setPassword] = useState('');
+  const [rememberMe, setRememberMe] = useState(false); 
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Enviando os dados: ${firstName} - ${password} - Lembrar Senha: ${rememberMe}`);
+  };
+
   return (
-    <div className="containerWrapper">
-      <div className="imagem">
-        <img src="./logo.png" alt="FINAE logo" className="logo" />
+    <div className="container">
+      <div className="login-left">
+        <div className="login-logo">
+          <img src="./logo.png" alt="FINAE logo" className="logo" />
+        </div>
       </div>
-      <div className="container">
-        <form>
-          <h1>Bem Vindo(a)!</h1>
-          <div className="inputBox">
-            <input placeholder="Usuário" type="email" />
+      <div className="login-right">
+        <form onSubmit={handleSubmit} className="login-form">
+          <h2 className="form-title">Bem vindo(a/e)</h2>
+          <div className="form-group">
+            <div className="input-group">
+              <input
+                type="text"
+                placeholder="Usuário"
+                onChange={(e) => setFirstname(e.target.value)}
+                className="form-input"
+              />
+            </div>
+            <div className="input-group">
+              <input
+                type="password"
+                placeholder="Senha"
+                onChange={(e) => setPassword(e.target.value)}
+                className="form-input"
+              />
+            </div>
+              <div className="remember-forgot">
+                <label>
+                  <input
+                    type="checkbox"
+                    checked={rememberMe}
+                    onChange={(e) => setRememberMe(e.target.checked)}
+                  />
+                  Lembrar Senha
+                </label>
+              </div>
           </div>
-          <div className="inputBox">
-            <input placeholder="Senha" type="password" />
-          </div>
-          <div className="rememberForgot">
-            <label>
-              <input type="checkbox" />
-              Lembrar Senha
-            </label>
-            <a href="#">Esqueci Senha</a>
-          </div>
-          <button type="submit" className="login">Entrar</button>
-          <div className="registerLink">
-            <p>Não tem uma conta? <a href="#">Cadastre-se</a></p>
+          <button type="submit" className="form-button">
+            Entrar
+          </button>
+          <div className="separator"></div>
+          <div className="form-links">
+            <a href="#" className="link">
+              Esqueceu a senha?
+            </a>
+            <a href="#" className="link">
+              Não tem uma conta? Cadastre-se!
+            </a>
           </div>
         </form>
       </div>
     </div>
   );
-};
+}
 
-export default Login;
+export default Login;
